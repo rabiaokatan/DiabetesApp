b361ebd28ad196074f41a16cfa0616133abbb297
Rabia Okatan <rabiaro26@gmail.com>
2021-11-09 23:25:30 +0300

    Nutritional Values table added

---
+++ lib/models/nutritional_values.dart
@@ -0,0 +1,55 @@
+class NutritionalValue{
+  int id;
+  String name;
+  String image;
+  
+  NutritionalValue({required this.id, required this.name, required this.image});
+}
+
+List<NutritionalValue> nutritionalValuesList=[
+NutritionalValue(
+  id:0,
+  name:'Tortilla Wrap',
+  image:'assets/images/tortilla-wrap.jpg',
+  ),
+  NutritionalValue(
+    id:1,
+    name:'Revani',
+    image:'assets/images/tortilla-wrap.jpg',
+  ),
+  NutritionalValue(
+    id:2,
+    name: 'Çikolatalı Mozaik Pasta',
+    image:'assets/images/tortilla-wrap.jpg',
+  ),
+  NutritionalValue(
+  id:3,
+  name:'Tortilla Wrap',
+  image:'assets/images/tortilla-wrap.jpg',
+  ),
+ NutritionalValue(
+    id:4,
+    name:'Revani',
+    image:'assets/images/tortilla-wrap.jpg',
+  ),
+  NutritionalValue(
+    id:5,
+    name: 'Çikolatalı Mozaik Pasta',
+    image:'assets/images/tortilla-wrap.jpg',
+  ),
+  NutritionalValue(
+  id:6,
+  name:'Tortilla Wrap',
+  image:'assets/images/tortilla-wrap.jpg',
+  ),
+NutritionalValue(
+    id:7,
+    name:'Revani',
+    image:'assets/images/tortilla-wrap.jpg',
+  ),
+  NutritionalValue(
+    id:8,
+    name: 'Çikolatalı Mozaik Pasta',
+    image:'assets/images/tortilla-wrap.jpg',
+  ),
+];
--- lib/screens/nutritional_values_screen.dart
+++ lib/screens/nutritional_values_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_diabetes_app/models/nutritional_values.dart';
 import 'package:flutter_diabetes_app/responsive.dart';
 import 'package:flutter_diabetes_app/screens/constants.dart';
+import 'package:flutter_diabetes_app/screens/home_screen.dart';
 import 'package:flutter_diabetes_app/widget/side_menu.dart';
 
 class NutritionalValuesScreen extends StatefulWidget {
@@ -38,23 +41,122 @@ class _NutritionalValuesScreenState extends State<NutritionalValuesScreen> {
           children: [
             isDesktop ? SideMenu() : Container(),
             Padding(
-               padding:isDesktop ? EdgeInsets.only(left:_size.width*0.7, top: _size.height*0.04, bottom:_size.height*0.03, right:_size.width*0.05 ) : EdgeInsets.symmetric(vertical: _size.height*0.03, horizontal:_size.width*0.05 ),
+              padding: isDesktop
+                  ? EdgeInsets.only(
+                      left: _size.width * 0.7,
+                      top: _size.height * 0.04,
+                      bottom: _size.height * 0.03,
+                      right: _size.width * 0.05)
+                  : EdgeInsets.symmetric(
+                      vertical: _size.height * 0.03,
+                      horizontal: _size.width * 0.05),
               child: Container(
-                width: _size.width * ( isDesktop ? 0.3 : 0.98),
+                width: _size.width * (isDesktop ? 0.3 : 0.98),
                 height: _size.height * 0.07,
                 decoration: BoxDecoration(
                   color: Colors.grey[300],
-                 borderRadius: BorderRadius.circular(isTablet ? 50 : 25),
+                  borderRadius: BorderRadius.circular(isTablet ? 50 : 25),
                 ),
-                child:  ListTile(
-                  leading: Icon(Icons.search, size: isMobile ? _size.width*0.05 : isTablet ? _size.width*0.04 : _size.width*0.015,),
-                  title:  TextField(
-                     decoration: InputDecoration(hintText: 'Ara...', hintStyle: TextStyle(color: Colors.grey[700], fontSize: isMobile ? _size.width*0.05 : isTablet ? _size.width*0.04 : _size.width*0.015 ), border: InputBorder.none),
-                     ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.search,
+                    size: isMobile
+                        ? _size.width * 0.05
+                        : isTablet
+                            ? _size.width * 0.04
+                            : _size.width * 0.015,
+                  ),
+                  title: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Ara...',
+                        hintStyle: TextStyle(
+                            color: Colors.grey[700],
+                            fontSize: isMobile
+                                ? _size.width * 0.05
+                                : isTablet
+                                    ? _size.width * 0.04
+                                    : _size.width * 0.015),
+                        border: InputBorder.none),
+                  ),
                 ),
               ),
             ),
-
+            Padding(
+              padding: isDesktop
+                  ? EdgeInsets.only(left: _size.width * 0.18)
+                  : EdgeInsets.symmetric(horizontal: _size.width * 0.02, vertical: _size.height*0.06),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: _size.height * 0.02),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade300)),
+                  child: Scrollbar(
+                    isAlwaysShown: true,
+                    controller: ScrollController(),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: DataTable(
+                        columnSpacing: isDesktop ? _size.width*0.1 : 56,
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text(
+                              "Besin",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Merriweather',
+                              ),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              "Karbonhidrat",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Merriweather',
+                              ),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              "Yağ",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Merriweather',
+                              ),
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              "Yağ",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Merriweather',
+                              ),
+                            ),
+                          ),
+                        ],
+                        rows: nutritionalValuesList.map((item) {
+                          return DataRow(
+                              color: MaterialStateProperty.resolveWith<Color>(
+                                  (Set<MaterialState> states) {
+                                if (item.id % 2 == 0)
+                                  return Colors.grey.shade300;
+                                return Colors
+                                    .transparent; // Use the default value.
+                              }),
+                              cells: [
+                                DataCell(Text(item.name)),
+                                DataCell(Center(child: Text(item.id.toString()))),
+                                DataCell(Center(child: Text(item.id.toString()))),
+                                DataCell(Center(child: Text(item.id.toString()))),
+                              ]);
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
