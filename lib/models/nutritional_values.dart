2e18a1418d931caec9e5a01433a8dae9b5b57243
Rabia Okatan <rabiaro26@gmail.com>
2021-11-13 15:35:42 +0300

    Nutritional values model completed

--- lib/models/nutritional_values.dart
+++ lib/models/nutritional_values.dart
@@ -1,55 +1,149 @@
-class NutritionalValue{
+class NutritionalValue {
   int id;
   String name;
-  String image;
-  
-  NutritionalValue({required this.id, required this.name, required this.image});
+  String calorie;
+  String fat;
+  String protein;
+  String totalCarbohydrate;
+  String cholesterol;
+  String sodium;
+  String potassium;
+  String calcium;
+
+  NutritionalValue({
+    required this.id,
+    required this.name,
+    required this.calorie,
+    required this.fat,
+    required this.protein,
+    required this.totalCarbohydrate,
+    required this.calcium,
+    required this.potassium,
+    required this.sodium,
+    required this.cholesterol,
+  });
 }
 
-List<NutritionalValue> nutritionalValuesList=[
-NutritionalValue(
-  id:0,
-  name:'Tortilla Wrap',
-  image:'assets/images/tortilla-wrap.jpg',
-  ),
+List<NutritionalValue> nutritionalValuesList = [
+
   NutritionalValue(
-    id:1,
-    name:'Revani',
-    image:'assets/images/tortilla-wrap.jpg',
-  ),
+    id: 0, 
+    name: "Yumurta", 
+    calorie: "155", 
+    fat: "11 g", 
+    protein: "13 g", 
+    totalCarbohydrate: "1,1 g", 
+    cholesterol:"373 mg" ,
+    sodium: "124 mg",
+    potassium: "126 mg",
+    calcium: "50 mg",
+    ),
   NutritionalValue(
-    id:2,
-    name: 'Çikolatalı Mozaik Pasta',
-    image:'assets/images/tortilla-wrap.jpg',
-  ),
+    id:1, 
+    name: "Ekmek", 
+    calorie: "264", 
+    fat: "3,2 g", 
+    protein: "9 g", 
+    totalCarbohydrate: "49 g", 
+    calcium: "260 mg", 
+    potassium: "115 mg", 
+    sodium: "491 mg",
+    cholesterol: "0 mg",
+    ),
   NutritionalValue(
-  id:3,
-  name:'Tortilla Wrap',
-  image:'assets/images/tortilla-wrap.jpg',
-  ),
- NutritionalValue(
-    id:4,
-    name:'Revani',
-    image:'assets/images/tortilla-wrap.jpg',
-  ),
+    id: 2, 
+    name: "Makarna", 
+    calorie: "131", 
+    fat: "1,1 g", 
+    protein: "5 g", 
+    totalCarbohydrate: "25 g", 
+    calcium: "6 mg", 
+    potassium: "24 mg", 
+    sodium: "6 mg",
+    cholesterol: "33 mg",
+    ),
   NutritionalValue(
-    id:5,
-    name: 'Çikolatalı Mozaik Pasta',
-    image:'assets/images/tortilla-wrap.jpg',
-  ),
+    id: 3, 
+    name: "Tavuk", 
+    calorie: "239", 
+    fat: "14 g", 
+    protein: "27 g", 
+    totalCarbohydrate: "0 g", 
+    calcium: "15 mg", 
+    potassium: "223 mg", 
+    sodium: "82 mg",
+    cholesterol: "88 mg",
+    ),
+     NutritionalValue(
+    id: 4, 
+    name: "Pilav", 
+    calorie: "359", 
+    fat: "1,4 g", 
+    protein: "10 g", 
+    totalCarbohydrate: "76 g", 
+    calcium: "83 mg", 
+    potassium: "188 mg", 
+    sodium: "1.303 mg",
+    cholesterol: "0 mg",
+    ),
+   NutritionalValue(
+    id: 5, 
+    name: "Patates Kızartması", 
+    calorie: "311", 
+    fat: "15 g", 
+    protein: "3,4 g", 
+    totalCarbohydrate: "41 g", 
+    calcium: "18 mg", 
+    potassium: "579 mg", 
+    sodium: "210 mg",
+    cholesterol: "0 mg",
+    ),
+   NutritionalValue(
+    id: 6, 
+    name: "Peynir", 
+    calorie: "402", 
+    fat: "33 g", 
+    protein: "25 g", 
+    totalCarbohydrate: "1,3 g", 
+    calcium: "721 mg", 
+    potassium: "98 mg", 
+    sodium: "621 mg",
+    cholesterol:"105 mg" ,
+    ),
+   NutritionalValue(
+    id: 7, 
+    name: "Zeytin", 
+    calorie: "115", 
+    fat: "11 g", 
+    protein: "0,8 g", 
+    totalCarbohydrate: "6 g", 
+    calcium: "88 mg", 
+    potassium: "8 mg", 
+    sodium: "735 mg",
+    cholesterol: "0 mg",
+    ),
   NutritionalValue(
-  id:6,
-  name:'Tortilla Wrap',
-  image:'assets/images/tortilla-wrap.jpg',
-  ),
-NutritionalValue(
-    id:7,
-    name:'Revani',
-    image:'assets/images/tortilla-wrap.jpg',
-  ),
+    id: 8, 
+    name: "Elma", 
+    calorie: "52", 
+    fat: "0,2 g", 
+    protein: "0,3 g", 
+    totalCarbohydrate: "14 g", 
+    calcium: "6 mg", 
+    potassium: "107 mg", 
+    sodium: "1 mg",
+    cholesterol: "0 mg",
+    ),
   NutritionalValue(
-    id:8,
-    name: 'Çikolatalı Mozaik Pasta',
-    image:'assets/images/tortilla-wrap.jpg',
-  ),
+    id: 9, 
+    name: "Muz", 
+    calorie: "88", 
+    fat: "0,3 g", 
+    protein: "1,1 g", 
+    totalCarbohydrate: "23 g", 
+    calcium: "5 mg", 
+    potassium: "358 mg", 
+    sodium: "1 mg",
+    cholesterol: "0 mg",
+    ),
 ];
--- lib/screens/nutritional_values_screen.dart
+++ lib/screens/nutritional_values_screen.dart
@@ -83,74 +83,130 @@ class _NutritionalValuesScreenState extends State<NutritionalValuesScreen> {
             ),
             Padding(
               padding: isDesktop
-                  ? EdgeInsets.only(left: _size.width * 0.18)
-                  : EdgeInsets.symmetric(horizontal: _size.width * 0.02, vertical: _size.height*0.06),
+                  ? EdgeInsets.only(left: _size.width * 0.20, right: _size.width*0.02, top: _size.height*0.05)
+                  : EdgeInsets.only(
+                      left: _size.width * 0.03,
+                      right: _size.width * 0.03,
+                      top: isTablet ? _size.height*0.01 : _size.height * 0.12,
+                      bottom: _size.height*0.02),
               child: Center(
                 child: Container(
                   padding: EdgeInsets.only(bottom: _size.height * 0.02),
                   decoration: BoxDecoration(
                       border: Border.all(color: Colors.grey.shade300)),
-                  child: Scrollbar(
-                    isAlwaysShown: true,
-                    controller: ScrollController(),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: DataTable(
-                        columnSpacing: isDesktop ? _size.width*0.1 : 56,
-                        columns: const <DataColumn>[
-                          DataColumn(
-                            label: Text(
-                              "Besin",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'Merriweather',
+                  child: SingleChildScrollView(
+                    child: Scrollbar(
+                      isAlwaysShown: true,
+                      controller: ScrollController(),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: DataTable(
+                          columnSpacing: isDesktop ? _size.width * 0.1 : 56,
+                          columns: const <DataColumn>[
+                            DataColumn(
+                              label: Text(
+                                "Besin (100g)",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
                               ),
                             ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              "Karbonhidrat",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'Merriweather',
+                            DataColumn(
+                              label: Text(
+                                "Kalori (kcal)",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
                               ),
                             ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              "Yağ",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'Merriweather',
+                            DataColumn(
+                              label: Text(
+                                "Yağ",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
                               ),
                             ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              "Yağ",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontFamily: 'Merriweather',
+                            DataColumn(
+                              label: Text(
+                                "Protein",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                        rows: nutritionalValuesList.map((item) {
-                          return DataRow(
-                              color: MaterialStateProperty.resolveWith<Color>(
-                                  (Set<MaterialState> states) {
-                                if (item.id % 2 == 0)
-                                  return Colors.grey.shade300;
-                                return Colors
-                                    .transparent; // Use the default value.
-                              }),
-                              cells: [
-                                DataCell(Text(item.name)),
-                                DataCell(Center(child: Text(item.id.toString()))),
-                                DataCell(Center(child: Text(item.id.toString()))),
-                                DataCell(Center(child: Text(item.id.toString()))),
-                              ]);
-                        }).toList(),
+                            DataColumn(
+                              label: Text(
+                                "Karbonhidrat (tümü)",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                "Kolestrol",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                "Sodyum",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                "Potasyum",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                "Kalsiyum",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Merriweather',
+                                ),
+                              ),
+                            ),
+                          ],
+                          rows: nutritionalValuesList.map((item) {
+                            return DataRow(
+                                color: MaterialStateProperty.resolveWith<Color>(
+                                    (Set<MaterialState> states) {
+                                  if (item.id % 2 == 0)
+                                    return Colors.grey.shade300;
+                                  return Colors
+                                      .transparent; // Use the default value.
+                                }),
+                                cells: [
+                                  DataCell(Text(item.name)),
+                                  DataCell(Center(child: Text(item.calorie))),
+                                  DataCell(Center(child: Text(item.fat))),
+                                  DataCell(Center(child: Text(item.protein))),
+                                  DataCell(Center(child: Text(item.totalCarbohydrate))),
+                                  DataCell(Center(child: Text(item.cholesterol))),
+                                  DataCell(Center(child: Text(item.sodium))),
+                                  DataCell(Center(child: Text(item.potassium))),
+                                  DataCell(Center(child: Text(item.calcium))),
+                                ]);
+                          }).toList(),
+                        ),
                       ),
                     ),
                   ),
